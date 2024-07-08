64a56e8e26d9b59d6c636f9643e56a74fee3edaf
jason-zhao-git <zjq233333@gmail.com>
2024-07-08 04:43:03 -0700

    main page nav layout done

--- app/loggedin/page.tsx
+++ app/loggedin/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     <main>
       <div className="relative min-h-screen min-w-full flex flex-col justify-center items-center bg-cover bg-center bg-gray-900">
         <div className="grid w-full md:grid-cols-[25%_1fr] lg:grid-cols-[40%_1fr]">
-          <div className="fixed top-1/4 md:left-1/10 right-0 z-50 flex justify-between items-center">
+          <div className="fixed top-1/7 md:left-1/10 right-0 z-50 flex justify-between items-center">
             <div className="lg:ml-16">
               <SideBar />
             </div>
