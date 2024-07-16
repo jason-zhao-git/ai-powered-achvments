5655797aed8559486dc0535ea46017f19d2ea975
jason-zhao-git <zjq233333@gmail.com>
2024-07-15 22:31:43 -0700

    working save/restore

--- components/demoflow/Tutorial.jsx
+++ components/demoflow/Tutorial.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import {
   ReactFlow,
   MiniMap,
@@ -20,9 +20,7 @@ import defaultEdges from "./Edges";
 import TaskNode from "./Nodes/TaskNode.jsx";
 import CustomNode from "./Nodes/CardNote.jsx";
 import AddNodeModal from "./AddNodeModal";
-import CustomControls from "./CustomControls.jsx"
-
-
+import CustomControls from "./CustomControls.jsx";
 
 // the following is everything about the flow bg in the tutorial page
 
@@ -35,16 +33,44 @@ function FlowComponent() {
   const [nodes, setNodes, onNodesChange] = useNodesState(defaultNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(defaultEdges);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [rfInstance, setRfInstance] = useState(null);
+
+
   const reactFlowInstance = useReactFlow();
+  const { setViewport } = useReactFlow();
+
+  const flowKey = "tutorial";
 
   const onConnect = useCallback(
     (connection) => {
-      console.log('Connection:', connection);
+      console.log("Connection:", connection);
       const edge = { ...connection, type: "smoothstep" };
       setEdges((eds) => addEdge(edge, eds));
     },
     [setEdges]
   );
+
+  const onSave = useCallback(() => {
+    if (rfInstance) {
+      const flow = rfInstance.toObject();
+      localStorage.setItem(flowKey, JSON.stringify(flow));
+    }
+  }, [rfInstance]);
+
+  const onRestore = useCallback(() => {
+    const restoreFlow = async () => {
+      const flow = JSON.parse(localStorage.getItem(flowKey));
+
+      if (flow) {
+        const { x = 0, y = 0, zoom = 1 } = flow.viewport;
+        setNodes(flow.nodes || []);
+        setEdges(flow.edges || []);
+        setViewport({ x, y, zoom });
+      }
+    };
+
+    restoreFlow();
+  }, [setNodes, setViewport]);
 
   const addNode = ({ name, nodeType, description, subtasks, connections }) => {
     const newNode = {
@@ -54,10 +80,10 @@ function FlowComponent() {
         description: description,
         subtasks: subtasks,
         connections: {
-          up: connections.up === 'none' ? null : connections.up,
-          down: connections.down === 'none' ? null : connections.down,
-          left: connections.left === 'none' ? null : connections.left,
-          right: connections.right === 'none' ? null : connections.right,
+          up: connections.up === "none" ? null : connections.up,
+          down: connections.down === "none" ? null : connections.down,
+          left: connections.left === "none" ? null : connections.left,
+          right: connections.right === "none" ? null : connections.right,
         },
         isCompleted: false,
         createdAt: new Date().toISOString(),
@@ -68,8 +94,6 @@ function FlowComponent() {
     };
     reactFlowInstance.setNodes((nds) => nds.concat(newNode));
   };
-
-
 
   return (
     <div className="h-full">
@@ -85,11 +109,16 @@ function FlowComponent() {
         edges={edges}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
+        onInit={setRfInstance}
         fitView
       >
         <CustomControls onAddNode={() => setIsModalOpen(true)} />
         <Background />
         <MiniMap nodeStrokeWidth={3} zoomable pannable />
+        <Panel position="top-right">
+        <button onClick={onSave}>save</button>
+        <button onClick={onRestore}>restore</button>
+        </Panel>
       </ReactFlow>
     </div>
   );
